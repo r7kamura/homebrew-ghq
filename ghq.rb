522110e355fe4d2aa2ffcbd290a837e51e9eb31a
Ryo Nakamura <r7kamura@gmail.com>
2014-07-30 00:54:40 +0900

    Use r7kamura/ghq's ghq binary
    Because the original ghq binary is broken for cross-compiling issue.

--- ghq.rb
+++ ghq.rb
@@ -3,8 +3,8 @@ require 'formula'
 HOMEBREW_GHQ_VERSION='0.4'
 class Ghq < Formula
   homepage 'https://github.com/motemen/ghq'
-  url "https://github.com/motemen/ghq/releases/download/v#{HOMEBREW_GHQ_VERSION}/ghq_darwin_amd64.zip"
-  sha1 '220d77560e6619d4d53c42c9c5c68d3c9fb03f5b'
+  url "https://github.com/r7kamura/ghq/releases/download/v#{HOMEBREW_GHQ_VERSION}/ghq_darwin_amd64.zip"
+  sha1 '527c6bfa4f4fc03b19cb5795ad4736cecfd6763f'
 
   version HOMEBREW_GHQ_VERSION
   head 'https://github.com/motemen/ghq', :using => :git, :branch => 'master'
@@ -24,6 +24,6 @@ class Ghq < Formula
       system 'make', 'BUILD_FLAGS=-o ghq'
     end
 
-    bin.install 'ghq'
+    bin.install 'ghq_darwin_amd64/ghq'
   end
 end
